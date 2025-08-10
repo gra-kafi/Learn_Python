bad43cb575444af27ffc401f855744a9dca305cb
gra-kafi <gra.kafi20@gmail.com>
2025-08-10 23:38:30 +0600

    add math

---
+++ Math/math.py
@@ -0,0 +1,24 @@
+import math
+pi = 3.14
+print(round(pi))
+
+print(math.ceil(pi))
+
+print(math.floor(pi))
+
+#absolute number
+print(abs(pi))
+
+#power
+print(pow(pi,2))
+
+#squre root
+print(math.sqrt(420))
+
+#largest value
+
+x = 1 , y = 2 , z = 3
+
+print(max(x,y,z))
+
+print(min(x,y,z))
